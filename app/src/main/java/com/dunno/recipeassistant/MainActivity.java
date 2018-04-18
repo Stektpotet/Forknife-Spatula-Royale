db13ba1ffaa000a92283df78e4bc16af1a890b6f
Halvor Bakken Smedås <stektpotet@gmail.com>
2018-04-18 20:16:43 +0200

    More design, added recipe list item

--- app/src/main/java/com/dunno/recipeassistant/MainActivity.java
+++ app/src/main/java/com/dunno/recipeassistant/MainActivity.java
@@ -2,9 +2,13 @@ package com.dunno.recipeassistant;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,4 +45,52 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public ConstraintLayout itemView;
+
+            public ViewHolder(ConstraintLayout v) {
+                super(v);
+                itemView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public ListAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+            // create a new view
+            ConstraintLayout item = (ConstraintLayout) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recipe_item, parent, false);
+            ViewHolder vh = new ViewHolder(item);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+//            holder.itemView.setText(mDataset[position]);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+
+    }
 }

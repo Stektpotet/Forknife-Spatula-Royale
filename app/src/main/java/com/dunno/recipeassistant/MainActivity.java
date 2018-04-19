0d2b12ebc3923c48df6a7a47b24df97e96dbe2a0
Halvor Bakken Smedås <stektpotet@gmail.com>
2018-04-19 13:45:50 +0200

    Added Adapters for main activity

--- app/src/main/java/com/dunno/recipeassistant/MainActivity.java
+++ app/src/main/java/com/dunno/recipeassistant/MainActivity.java
@@ -4,46 +4,56 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    public static final String TAG = MainActivity.class.getName();
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private TabPagerAdapter mTabPagerAdapter;
+    private ProgressBar mProgressBar;
+    private LockedViewPager mViewPager;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.main_navigationBar);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setupUI();
     }
+
+    private void setupUI() {
+
+        mViewPager = findViewById(R.id.main_viewPager);
+        BottomNavigationView navigation = findViewById(R.id.main_navigationBar);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mTabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager.setAdapter(mTabPagerAdapter);
+        mViewPager.setCurrentItem(R.id.navigation_home);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            mViewPager.setCurrentItem(item.getItemId());
+            return true;
+        }
+    };
 
     public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         private String[] mDataset;
@@ -92,5 +102,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class TabPagerAdapter extends FragmentPagerAdapter {
+
+        public TabPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case R.id.navigation_home:
+                    return RecipeListFragment.newInstance(0);
+                case R.id.navigation_dashboard:
+                    return RecipeListFragment.newInstance(0);
+                case R.id.navigation_notifications:
+                    return RecipeListFragment.newInstance(0);
+            }
+            return RecipeListFragment.newInstance(0);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case R.id.navigation_home:
+                    return "Recipes"; //TODO: put these into @values/strings
+                case R.id.navigation_dashboard:
+                    return "Fridge";
+                case R.id.navigation_notifications:
+                    return "Shopping List";
+            }
+            return "How did you get here?";
+        }
     }
 }
